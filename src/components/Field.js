2acf23becde3da0b05d1d297a3d278deb8e68268
Natalia Kulas <nataliakulas@gmail.com>
2018-04-30 08:19:35 +0200

    details of counter initial version

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export default (props) =>
-  <div className={`field ${props.display ? props.display : ""}`} style={{margin: `${props.margin ? props.margin : ""}` }}>
+  <div className={`field ${props.display ? props.display : ""}`}
+       style={{margin: `${props.margin ? props.margin : ""}`, padding: `${props.padding ? props.padding : ""}`}}>
     {props.legend ?
       <h2 className="field-legend">{props.legend}</h2> : null}
     {props.children}
--- src/pages/details.js
+++ src/pages/details.js
@@ -5,24 +5,91 @@ import { Container, Row, Col } from 'react-grid-system';
 
 import withAuthorization from '../helpers/withAuthorization';
 import { authCondition } from '../helpers/helpers';
+import { db } from '../firebase';
+
+import Field from '../components/Field';
+import Button from '../components/Button';
+import moment from "moment/moment";
 
 class CounterDetails extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      counter: '',
+      format: "seconds"
+    }
   }
 
   componentWillMount() {
+    db.getCounters().then(snap => {
+      const counters = [];
 
+      snap.forEach(item => {
+        let counter = item.val();
+        counter.key = item.key;
+
+        counters.push(counter)
+      });
+      console.log(counters);
+
+      counters.forEach(counter => {
+        console.log(counter.key)
+        if (counter.key === this.props.match.params.id) {
+          this.setState({counter: counter})
+        }
+      })
+    })
   }
 
+  changeFormat = (e) => {
+    this.setState({format: e.target.value})
+  };
+
   render() {
-    const id = this.props.match.params.id
+    const now = moment();
+    const timer = moment(this.state.counter.timestamp);
+    const formatWeekday = "dddd";
+    const formatDate = "DD MMMM YYYY";
+    const formatTime = "HH:mm";
+
+    const formats = ["seconds", "minutes", "hours", "days", "months", "years"];
 
     return (
       <Container>
         <Row>
           <Col xs={12}>
-            {id}
+            <Field legend={this.state.counter.name ? this.state.counter.name : "Waiting for counter.."}
+                   margin="10% 0 0 0" padding="50px 60px">
+              <Row>
+                <Col xs={6}>
+                  <div className="counter-box">
+                    <div className="half top">
+                      <p className="bold">Counter set to:</p>
+                      <div>
+                        <p>{timer.format(formatWeekday)},</p>
+                        <p>{timer.format(formatDate)}</p>
+                        <p>{timer.format(formatTime)}</p>
+                      </div>
+                    </div>
+                    <div className="half bottom">
+                      <p className="bold">Remaining time:</p>
+                      <div>
+                        <p>{timer.diff(now, this.state.format)}</p>
+                        <select value={this.state.format} onChange={e => this.changeFormat(e)}
+                                className="counter-format">
+                          {formats.map((format, i) => {
+                            return <option key={i} value={format}>{format}</option>
+                          })}
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={6} className="column center">
+                  <Button>Edit</Button>
+                </Col>
+              </Row>
+            </Field>
           </Col>
         </Row>
       </Container>
@@ -31,5 +98,6 @@ class CounterDetails extends React.Component {
 }
 
 export default compose(
-  withAuthorization(authCondition)
-)(withRouter(CounterDetails))
+  withAuthorization(authCondition),
+  withRouter
+)(CounterDetails)
